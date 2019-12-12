7427d8806ff31ab5f70e1e35f9a1fdd12006ef48
Roan van Staveren <38979087+roanvanstaveren@users.noreply.github.com>
2019-12-12 17:21:28 +0100

    added changes

--- ID3_Tag_Editor/ID3_Tag_Editor/frmMain.xaml.cs
+++ ID3_Tag_Editor/ID3_Tag_Editor/frmMain.xaml.cs
@@ -27,7 +27,7 @@ namespace ID3_Tag_Editor
     public partial class frmMain : Window
     {
         private ID3Info _Data;
-        ObservableCollection<ID3Info> collection = new ObservableCollection<ID3Info>();
+        public ObservableCollection<ID3Info> collection = new ObservableCollection<ID3Info>();
         private int _selectedIndex;
         public int SelectedIndex
         {
@@ -50,47 +50,71 @@ namespace ID3_Tag_Editor
             List.ValidateNames = true;
             if (List.ShowDialog() == true)
             {
-                var list = List.FileName;
+                string[] list = List.FileNames;
                 LoadM3u(list);
             }
         }
 
         // Load List 2
-        public void LoadM3u(string FilePath)
+        public void LoadM3u(string[] FilePaths)
         {
-            if (Path.GetExtension(FilePath).ToLower() != ".m3u")
+            if (Path.GetExtension(FilePaths[0]).ToLower() != ".m3u")
                 return;
 
             //ClearList();
-            string[] L = M3U.Load(FilePath);
-            foreach (string path in L)
+            string[] L = M3U.Load(FilePaths[0]);
+            AddNewFiles(L);
+        }
+
+
+        // Adding new file(s) to datagrid
+        public void AddNewFiles(string[] FilePaths)
+        {
+            ID3Info ID3File;
+            int i = 0;
+
+            try
             {
-                if (path == String.Empty)
-                    break;
-                AddNewFile(path);
+                foreach (string path in FilePaths)
+                {
+                    var file = TagLib.File.Create(FilePaths[i]);
+                    var fileInfo = new FileInfo(FilePaths[i]);
+                    ID3File = new ID3Info(FilePaths[i], true);
+                    ID3File.FileSize = fileInfo.Length / 1000;
+                    ////ID3File.ID3Size = file.
+                    //collection.Add(ID3File); // Filepath is set correctly, Columns are being added at the end
+                    //_Data = ID3File;
+                    //dataGrid.ItemsSource = collection;
+                    // FileInfo(path).Length
+                    i++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(FilePaths[i] + "\nCan't load file. " + ex.Message, "Loading File");
+                return;
             }
         }
 
-        // Adding new file(s) to datagrid
-        public void AddNewFile(string FilePath)
+        // Adding new file to datagrid
+        public void AddNewFile(string filePath)
         {
             ID3Info ID3File;
 
             try
             {
-                var file = TagLib.File.Create(FilePath);
-                var fileInfo = new FileInfo(FilePath);
-                ID3File = new ID3Info(FilePath, true);
+                var file = TagLib.File.Create(filePath);
+                var fileInfo = new FileInfo(filePath);
+                ID3File = new ID3Info(filePath, true);
                 ID3File.FileSize = fileInfo.Length / 1000;
-                //ID3File.ID3Size = file.
+                ////ID3File.ID3Size = file.
                 collection.Add(ID3File); // Filepath is set correctly, Columns are being added at the end
                 _Data = ID3File;
                 dataGrid.ItemsSource = collection;
-                // FileInfo(path).Length
             }
             catch (Exception ex)
             {
-                MessageBox.Show(FilePath + "\nCan't load file. " + ex.Message, "Loading File");
+                MessageBox.Show(filePath + "\nCan't load file. " + ex.Message, "Loading File");
                 return;
             }
         }
@@ -117,12 +141,15 @@ namespace ID3_Tag_Editor
         private void File_Add_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog List = new OpenFileDialog();
-            List.Multiselect = false;
+            List.Multiselect = true;
             List.Filter = "MP3 Files (*.mp3)|*.mp3";
             List.ValidateNames = true;
             if (List.ShowDialog() == true)
             {
-
+                if (List.FileNames.Length > 1)
+                {
+                    LoadMultipleMP3(List.FileNames);
+                }
                 LoadMP3(List.FileName);
             }
 
@@ -135,9 +162,27 @@ namespace ID3_Tag_Editor
             var file = TagLib.File.Create(path); // Get file from filepath
 
             if (Path.GetExtension(file.Name).ToLower() != ".mp3")
+            {
+                MessageBox.Show("File is not MP3.");
                 return;
+            }
 
             AddNewFile(path);
+        }
+
+        public void LoadMultipleMP3(string[] filePaths)
+        {
+            var file = TagLib.File.Create(path); // Get file from filepath
+
+            foreach (string item in filePaths)
+            {
+                if (Path.GetExtension(item.Name).ToLower() != ".mp3")
+                {
+                    MessageBox.Show("File is not MP3.");
+                    return; 
+                }
+            }
+            AddNewFiles(filePaths);
         }
 
         private void Save_List_Click(object sender, RoutedEventArgs e)
